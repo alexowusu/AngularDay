c9fcaab7522e92654835839654b40b93f9fe52e6
alexowusu <owusu3y@gmail.com>
2019-12-03 17:57:49 +0000

    day1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ClientListComponent } from './client-list/client-list.component';
+import { AddClientComponent } from './add-client/add-client.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClientListComponent,
+    AddClientComponent
   ],
   imports: [
     BrowserModule
---
+++ src/assets/b_files/videos.spice.js.download
@@ -0,0 +1,1 @@
+(function(env){"use strict";env.ddg_spice_videos=function(apiResult){if(!apiResult||!apiResult.results||!apiResult.results.length){return Spice.failed("videos")}Spice.add({id:"videos",name:"Videos",allowMultipleCalls:true,data:apiResult.results,model:"Video",templates:{group:"videos"},relevancy:{dup:"id"}})}})(this);
